e23402a63b33d56e94959a3efb18b5b38f7ff80d
jorge <jatovar@JATOVAR>
2017-04-05 16:43:48 -0500

    Add project files.

---
+++ src/Design-Patterns-CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_CSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
